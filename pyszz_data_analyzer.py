209ae1808284823950fa3d8dd0ca0809a7511a20
Omixxx <xxcortex99@gmail.com>
2023-01-12 16:33:38 +0100

    removed unecessary controlls

--- pyszz_data_analyzer.py
+++ pyszz_data_analyzer.py
@@ -2,18 +2,6 @@ import sys
 import json
 
 
-def is_json(file):
-    try:
-        with open(file, 'r') as f:
-            json_data = json.load(f)
-            if json_data:
-                return True
-            else:
-                return False
-    except ValueError:
-        return False
-
-
 def main(data_report):
     try:
         with open(data_report, 'r') as f:
@@ -21,7 +9,7 @@ def main(data_report):
             for item in json_data:
                 print(item)
     except ValueError:
-        print("something went wrong")
+        print("file is not json format")
         sys.exit(1)
 
 
@@ -30,10 +18,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("usage: python pyszz_data_analyzer.py \
                 <path/to/pyszz/output.json>")
-        sys.exit(1)
-
-    if not is_json(sys.argv[1]):
-        print("error: the file is not a json file")
         sys.exit(1)
 
     main(sys.argv[1])
